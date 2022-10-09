f80dcbfea0778cb7a937fbac053c9d5459a49717
niedermansam <niederman.sam@gmail.com>
2022-10-09 09:16:54 -0600

    create update-cache route

--- routes/files/index.js
+++ routes/files/index.js
@@ -11,7 +11,7 @@ const fileAirbaseId = 'tbl6bhoTJLFsQLy25';
 const errorMessage =  'Sorry, something went wrong. Please try again.';
 
 
-const cacheNames = {
+const fileCacheNames = {
   loaded: 'fileData',
   array: 'fileArray',
   object: 'fileObject',
@@ -23,15 +23,15 @@ module.exports = async function (fastify, opts) {
 
     const {output} = req.query;
 
-    let lookupResources = {};
-    let resourcesArray = [];
+    let lookupFiles = {};
+    let fileArray = [];
 
     let queryResponse = errorMessage
 
-    if (fastify.cache().has(cacheNames.loaded)) {
+    if (fastify.cache().has(fileCacheNames.loaded)) {
       
-      if (output === 'lookup')  queryResponse = fastify.cache().get(cacheNames.object)
-      else return  queryResponse = fastify.cache().get(cacheNames.array)
+      if (output === 'lookup')  queryResponse = fastify.cache().get(fileCacheNames.object)
+      else return  queryResponse = fastify.cache().get(fileCacheNames.array)
 
     } else {
       
@@ -47,15 +47,15 @@ module.exports = async function (fastify, opts) {
           let currentRecord = record.fields;
           currentRecord.id = record.id;
           
-          lookupResources[currentRecord.id] = currentRecord;
-          resourcesArray.push(currentRecord)
+          lookupFiles[currentRecord.id] = currentRecord;
+          fileArray.push(currentRecord)
         });
 
-        fastify.cache().set(cacheNames.object, lookupResources);
-        fastify.cache().set(cacheNames.array, resourcesArray);
-        fastify.cache().set(cacheNames.loaded, true);
+        fastify.cache().set(fileCacheNames.object, lookupFiles);
+        fastify.cache().set(fileCacheNames.array, fileArray);
+        fastify.cache().set(fileCacheNames.loaded, true);
         
-        queryResponse = output == 'lookup' ? lookupResources : resourcesArray;
+        queryResponse = output == 'lookup' ? lookupFiles : fileArray;
       });
     }
     return res.send(queryResponse);
